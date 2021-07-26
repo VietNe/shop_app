75a797530802bb35173667242c83756ea3d1a321
JokerV <Quocvietcoi2000@gmail.com>
2021-07-26 15:48:41 +0700

    task: add stacks for bottom bar

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -14,7 +14,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
-
+  List<int> stacks = [0];
   static const List<Widget> _widgetOptions = <Widget>[
     HomeScreen(),
     HomeScreen(),
@@ -26,23 +26,44 @@ class _MainScreenState extends State<MainScreen> {
     setState(() {
       _selectedIndex = index;
     });
+    if (!stacks.contains(index)) {
+      stacks.add(index);
+    } else {
+      stacks.remove(index);
+      stacks.add(index);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-
-    return Scaffold(
-      body: Stack(
-        children: [
-          _widgetOptions.elementAt(_selectedIndex),
-          Positioned(
-            bottom: 30,
-            right: SizeConfig.screenWidth * 0.1,
-            child: CustomBottomNavBar(
-                onItemTap: _onItemTapped, selectedIndex: _selectedIndex),
-          )
-        ],
+    print(stacks);
+    return WillPopScope(
+      onWillPop: () async {
+        if (stacks.isNotEmpty) {
+          if (stacks[stacks.length - 1] != 0) {
+            stacks.removeLast();
+            setState(() {
+              _selectedIndex = stacks[stacks.length - 1];
+            });
+            return false;
+          }
+          return true;
+        }
+        return true;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            _widgetOptions.elementAt(_selectedIndex),
+            Positioned(
+              bottom: 30,
+              right: SizeConfig.screenWidth * 0.1,
+              child: CustomBottomNavBar(
+                  onItemTap: _onItemTapped, selectedIndex: _selectedIndex),
+            )
+          ],
+        ),
       ),
     );
   }
